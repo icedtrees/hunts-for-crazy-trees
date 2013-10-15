47af43d327a7cee80497088dbb444305122c377d
Leo Tian Xing Huang <ltxh559@cse.unsw.EDU.AU>
2013-10-15 16:42:37 +1100

    Add base .c/.h files for hunter

---
+++ hunter.c
@@ -0,0 +1,15 @@
+#include "game.h"
+#include "HunterView.h"
+#include "hunter.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void decideMove (HunterView gameState) {
+
+    registerBestPlay("ZU", "I Wanna Stake You Dracula <3" );
+ 
+}
+
